c9fb91affcb959e63eb59a6934e273dd0a51d3fc
butterdogg1467 <momink2600@gmail.com>
2024-03-15 16:04:40 -0400

    Added content to portfolio, services, and aboutus
    Moved "about us" section from index.html to aboutus.html
    Added fourth project to portfolio
    Linked portfolio to "see more" button in index.html
    Made new stylesheet for portfolio.html

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 let portfolioImg1 = document.querySelector('#portfolioimg1')
 let portfolioImg2 = document.querySelector('#portfolioimg2')
 let portfolioImg3 = document.querySelector('#portfolioimg3')
+let portfolioImg4 = document.querySelector('#portfolioimg4')
 
 portfolioImg1.addEventListener('mouseenter', function() {
     portfolioImg1.style.animation = 'shadow-pop-tr1 0.3s ease forwards';
@@ -26,6 +27,14 @@ portfolioImg3.addEventListener('mouseenter', function() {
 
 portfolioImg3.addEventListener('mouseleave', function() {
     portfolioImg3.style.animation = 'shadow-pop-tr-reverse3 0.3s ease reverse';
+})
+
+portfolioImg4.addEventListener('mouseenter', function() {
+    portfolioImg4.style.animation = 'shadow-pop-tr1 0.3s ease forwards';
+})  
+
+portfolioImg4.addEventListener('mouseleave', function() {
+    portfolioImg4.style.animation = 'shadow-pop-tr-reverse1 0.3s ease reverse';
 })
 
 })
