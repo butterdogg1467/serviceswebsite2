e8df045fb8c8198be4f777efad065dd0d548e023
butterdogg1467 <momink2600@gmail.com>
2024-03-16 16:55:56 -0400

    Removed diffrent colors for animations for simplicity

--- script.js
+++ script.js
@@ -6,35 +6,35 @@ let portfolioImg3 = document.querySelector('#portfolioimg3')
 let portfolioImg4 = document.querySelector('#portfolioimg4')
 
 portfolioImg1.addEventListener('mouseenter', function() {
-    portfolioImg1.style.animation = 'shadow-pop-tr1 0.3s ease forwards';
+    portfolioImg1.style.animation = 'shadow-pop-tr 0.3s ease forwards';
 })  
 
 portfolioImg1.addEventListener('mouseleave', function() {
-    portfolioImg1.style.animation = 'shadow-pop-tr-reverse1 0.3s ease reverse';
+    portfolioImg1.style.animation = 'shadow-pop-tr-reverse 0.3s ease reverse';
 })  
 
 portfolioImg2.addEventListener('mouseenter', function() {
-    portfolioImg2.style.animation = 'shadow-pop-tr2 0.3s ease forwards';
+    portfolioImg2.style.animation = 'shadow-pop-tr 0.3s ease forwards';
 })  
 
 portfolioImg2.addEventListener('mouseleave', function() {
-    portfolioImg2.style.animation = 'shadow-pop-tr-reverse2 0.3s ease reverse';
+    portfolioImg2.style.animation = 'shadow-pop-tr-reverse 0.3s ease reverse';
 })
 
 portfolioImg3.addEventListener('mouseenter', function() {
-    portfolioImg3.style.animation = 'shadow-pop-tr3 0.3s ease forwards';
+    portfolioImg3.style.animation = 'shadow-pop-tr 0.3s ease forwards';
 })  
 
 portfolioImg3.addEventListener('mouseleave', function() {
-    portfolioImg3.style.animation = 'shadow-pop-tr-reverse3 0.3s ease reverse';
+    portfolioImg3.style.animation = 'shadow-pop-tr-reverse 0.3s ease reverse';
 })
 
 portfolioImg4.addEventListener('mouseenter', function() {
-    portfolioImg4.style.animation = 'shadow-pop-tr1 0.3s ease forwards';
+    portfolioImg4.style.animation = 'shadow-pop-tr 0.3s ease forwards';
 })  
 
 portfolioImg4.addEventListener('mouseleave', function() {
-    portfolioImg4.style.animation = 'shadow-pop-tr-reverse1 0.3s ease reverse';
+    portfolioImg4.style.animation = 'shadow-pop-tr-reverse 0.3s ease reverse';
 })
 
 })
